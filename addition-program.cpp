7720f79f9006acf7fcc1bf2d288f500f90e96f3e
HammadMaqbool <hammad.mqbl@gmail.com>
2025-03-21 08:30:20 -0700

    Two Programs Added
    I have added programs in this commit one is for the addition purpose and the 2nd one is for loop

---
+++ addition-program.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int valueOne;
+	int valueTwo;
+	
+	cout<<"Please enter value number one";
+	cin>>valueOne;
+	
+	cout<<"Please enter value number two";
+	cin>>valueTwo;
+	
+	cout<<"The Result is "<<valueOne+valueTwo;
+}
